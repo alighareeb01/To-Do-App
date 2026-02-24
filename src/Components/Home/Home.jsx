a6342d8bd9b9083878723c55a75b6c47c0ea28de
alighareeb01 <ghareeb874@gmail.com>
2026-02-24 10:37:23 +0200

    implemented create functionality

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -13,6 +13,8 @@ export default function Home() {
   const [toDos, setToDos] = useState([]);
   let [currentToDo, setCurrentToDo] = useState({});
 
+  let [updateTask, setUpdateTask] = useState(false);
+
   let schema = z.object({
     title: z.string().min(3, "at least 3").max(30, "at max 30"),
     description: z.string().min(3, "at least 3").max(30, "at max 30"),
@@ -24,7 +26,34 @@ export default function Home() {
     },
     resolver: zodResolver(schema),
   });
+  let updateForm = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+    resolver: zodResolver(schema),
+  });
+
+  function onSubmit(data) {
+    if (updateTask) {
+      handleUpdate(data);
+    } else {
+      handleCreate(data);
+    }
+  }
+
+  //  console.log(updateForm.register);
+
+  console.log(updateForm);
+
   let { register, handleSubmit, formState, reset } = createForm;
+
+  let {
+    register: register2,
+    handleSubmit: handleSubmit2,
+    formState: formState2,
+    reset: resetForm,
+  } = updateForm;
 
   function handleCreate(data) {
     setIsloading(true);
@@ -70,27 +99,112 @@ export default function Home() {
 
   function handleDelete(id) {
     // console.log(id);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Perform the delete request
+        axios
+          .delete(`https://todo-nti.vercel.app/todo/delete-todo/${id}`, {
+            headers: {
+              token: localStorage.getItem("userToken"),
+            },
+          })
+          .then((res) => {
+            // Success feedback
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your item has been deleted.",
+              icon: "success",
+            });
+            toast.success("Deleted successfully");
+            getAllToDo();
+          })
+          .catch((err) => {
+            // Error feedback
+            Swal.fire({
+              title: "Error!",
+              text: "Something went wrong.",
+              icon: "error",
+            });
+            toast.error("Something went wrong");
+            console.log(err);
+            getAllToDo();
+          });
+      }
+    });
+    // Swal.fire("You sure you want to delete")
+    //   .then(() => {
+    //     axios
+    //       .delete(`https://todo-nti.vercel.app/todo/delete-todo/${id}`, {
+    //         headers: {
+    //           token: localStorage.getItem("userToken"),
+    //         },
+    //       })
+    //       .then((res) => {
+    //         console.log(res);
+    //         toast.success("Deleted successfully");
+    //         getAllToDo();
+    //       })
+    //       .catch((err) => {
+    //         console.log(err);
+    //         toast.error("Something went wrong");
+    //         getAllToDo();
+    //       });
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  }
+
+  function handleSetup(todo) {
+    resetForm({
+      title: todo.title,
+      description: todo.description,
+    });
+
+    // resetForm({ title: todo.title });
+
+    setCurrentToDo(todo);
+    setUpdateTask(true);
+  }
+
+  function handleUpdate(data) {
+    // setIsloading(true);
+
+    // setCurrentToDo(todo);
+    console.log(currentToDo);
     axios
-      .delete(`https://todo-nti.vercel.app/todo/delete-todo/${id}`, {
-        headers: {
-          token: localStorage.getItem("userToken"),
+      .patch(
+        `https://todo-nti.vercel.app/todo/update-todo/${currentToDo._id}`,
+        data,
+
+        {
+          headers: {
+            token: localStorage.getItem("userToken"),
+          },
         },
-      })
+      )
       .then((res) => {
         console.log(res);
-        toast.success("Deleted successfully");
+        toast.success("updated");
+        setUpdateTask(false);
+        resetForm();
         getAllToDo();
       })
       .catch((err) => {
         console.log(err);
         toast.error("Something went wrong");
+        // setUpdateTask(false);
         getAllToDo();
       });
-  }
-
-  function handleEdit(todo) {
-    setCurrentToDo(todo);
-    console.log(currentToDo);
   }
   return (
     <>
@@ -101,7 +215,11 @@ export default function Home() {
             <div className="container mx-auto p-5 m-5">
               <form
                 className="max-w-sm mx-auto"
-                onSubmit={handleSubmit(handleCreate)}
+                onSubmit={
+                  updateTask
+                    ? handleSubmit2(handleUpdate)
+                    : handleSubmit(handleCreate)
+                }
               >
                 <div className="mb-5">
                   <label
@@ -115,7 +233,9 @@ export default function Home() {
                     id="title"
                     className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
                     required
-                    {...register("title")}
+                    // {...register("title")}
+                    // {updateTask && {...register2("title")}}
+                    {...(updateTask ? register2("title") : register("title"))}
                   />
                   {formState.errors.title && (
                     <p className="messageRed ">
@@ -135,7 +255,11 @@ export default function Home() {
                   rows="4"
                   className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body"
                   placeholder="Write your thoughts here..."
-                  {...register("description")}
+                  // {...register("description")}
+                  //  {...register2("description")}
+                  {...(updateTask
+                    ? register2("description")
+                    : register("description"))}
                 ></textarea>
                 {formState.errors.description && (
                   <p className="messageRed ">wrong description </p>
@@ -171,14 +295,20 @@ export default function Home() {
                     <p className="text-body">{todo.description}</p>
                     <div className="btns flex justify-center gap-5 mt-3">
                       <button
-                        onClick={() => handleDelete(todo._id)}
+                        onClick={() => {
+                          handleDelete(todo._id);
+                        }}
                         type="button"
                         className="text-white bg-brand px-4 py-2 rounded hover:bg-brand-strong"
                       >
                         Delete
                       </button>
                       <button
-                        onClick={() => handleEdit(todo)}
+                        onClick={() => {
+                          handleSetup(todo);
+                          setUpdateTask(true);
+                          // handleUpdate(todo);
+                        }}
                         type="button"
                         className="text-white bg-brand px-4 py-2 rounded hover:bg-brand-strong"
                       >
